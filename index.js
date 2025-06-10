4417aa4b19b81777637623c39c94e239d56a10ce
Rishab Thakur <coolrishab990@gmail.com>
2025-06-11 01:47:45 +0530

    Added security patch

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { Mongodburl } from './config.js';
 import userauth from './routes/auth.js';
 import cookieParser from 'cookie-parser';
 import restrictToLogInuser from './Middleware/authorizeUserByCookie.js';
 import shortIdGen from './routes/url.js';
 import staticRoute from './routes/staticRoute.js';
 import path from 'path';
+import dotenv from 'dotenv';
 
 
 const app=express();
 const port=4000;
+dotenv.config();
 
 app.set("view engine", "ejs");
 app.set('views', path.resolve("./views"));
@@ -23,7 +24,7 @@ app.use('/auth', userauth);
 app.use('/url',restrictToLogInuser,shortIdGen);
 app.use('/home', staticRoute);
 
-mongoose.connect(Mongodburl)
+mongoose.connect(process.env.MONGODB_URL)
 .then(function() {
     console.log('The Database is connected');
     app.listen(port, function() {
